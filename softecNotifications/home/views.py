49f2830b0b8990b59132a9ce7d746f238f02f164
Luis Naranjo <luisnaranjo733@gmail.com>
2013-03-04 18:30:29 -0800

    Added login bar and twitter bootstrap

--- softecNotifications/home/views.py
+++ softecNotifications/home/views.py
@@ -2,5 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'base.html')
-    return HttpResponse('hallo')
+    return render(request, 'index.html')
