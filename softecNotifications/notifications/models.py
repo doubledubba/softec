36807b615ed0310cb6c58494895102520836a275
Luis Naranjo <luisnaranjo733@gmail.com>
2013-03-15 19:02:32 -0700

    making some very skeptical progress

--- softecNotifications/notifications/models.py
+++ softecNotifications/notifications/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class RestrictedHoursModel(models.Model):
+    startHours = models.DateTimeField("Start hours")
+    endHours = models.DateTimeField("End hours")
+    
+    class Meta:
+        abstract = True
+
+class BaseContact(RestrictedHoursModel):
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    phone = models.CharField(max_length=11)
+    
+    class Meta:
+        abstract = True
+        
+    def __unicode__(self):
+        return self.name
+        
+class Agent(BaseContact):
+    pass
+    
+class Owner(BaseContact):
+    pass
+    
