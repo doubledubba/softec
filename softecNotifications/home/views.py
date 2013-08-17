63c90212fc7aa4f32e44f3b12c6652b4292de284
Luis Naranjo <luisnaranjo733@gmail.com>
2013-08-16 21:36:17 -0700

    implemented message alert system

--- softecNotifications/home/views.py
+++ softecNotifications/home/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate, logout, login
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'alert': 'success', 'message': 'fuck yeah', 'message_title': 'bitches'})
     
 def logout_user(request):
     logout(request)
@@ -14,4 +14,17 @@ def login_user(request):
     if request.method == 'GET':
         return render(request, 'sign_in.html')
     elif request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                # Redirect to a success page.
+            else:
+                pass
+                # Return a 'disabled account' error message
+        else:
+            pass
+            # Return an 'invalid login' error message.
         return HttpResponse('post')
