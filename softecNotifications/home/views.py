dc23328feb55f0ec091c278736215c8af217cac3
Luis Naranjo <luisnaranjo733@gmail.com>
2013-08-16 21:49:18 -0700

    auth and ux message systems work

--- softecNotifications/home/views.py
+++ softecNotifications/home/views.py
@@ -1,14 +1,27 @@
+from urllib import urlencode
+
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, logout, login
 
+GetifyUrl = lambda url, mapping: url + '?' + urlencode(mapping)
 
 def index(request):
-    return render(request, 'index.html', {'alert': 'success', 'message': 'fuck yeah', 'message_title': 'bitches'})
+    params = {
+        'alert': request.GET.get('alert'),
+        'message_title': request.GET.get('message_title'),
+        'message': request.GET.get('message'),
+    }
+    return render(request, 'index.html', params)
     
 def logout_user(request):
     logout(request)
-    return redirect('/')
+    mapping = {'alert': 'info',
+        'message_title': 'Bye bye!',
+        'message': 'You have succesfully logged out!'
+    }
+    url = GetifyUrl('/', mapping)
+    return redirect(url)
     
 def login_user(request):
     if request.method == 'GET':
@@ -20,11 +33,22 @@ def login_user(request):
         if user is not None:
             if user.is_active:
                 login(request, user)
+                mapping = {'alert': 'success', 
+                    'message_title': 'Success!', 
+                    'message': 'You have successfully logged in!'
+                }
                 # Redirect to a success page.
             else:
-                pass
+                mapping = {'alert': 'danger',
+                    'message_title': 'Uh oh!',
+                    'message': 'Your account has been disabled!'
+                }
                 # Return a 'disabled account' error message
         else:
-            pass
+            mapping = {'alert': 'danger',
+                'message_title': 'Woops!',
+                'message': 'Your login information is incorrect. Try again!'
+            }
             # Return an 'invalid login' error message.
-        return HttpResponse('post')
+        url = GetifyUrl('/', mapping)
+        return redirect(url)
